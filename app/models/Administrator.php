4dfdec28ddd908d51a58c8c97504936199be0040
_Aress_ <mohamed.anattahe@gmail.com>
2025-06-10 21:40:10 +0100

    Fixed folder capitalization for proper case sensitivity between Windows and Linux environments

--- app/models/Administrator.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace App\Models;
-
-class Administrator extends Model
-{
-    // We don't need to define any additional methods
-    // as the base Model class provides all we need for now
-}
